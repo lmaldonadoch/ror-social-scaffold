a950c3c80ee2573ddeef822ced9c4e7cc765a277
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-26 11:53:58 -0500

    Added the pending invites and pending friend requests functionality to the users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.ordered_by_most_recent
+    @posts = @user.posts.includes(:comments).ordered_by_most_recent
   end
 
   def invitation
