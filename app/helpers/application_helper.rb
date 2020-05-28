e04392e9606cad5316b1d1ca204ac87fe803b120
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-28 13:06:55 -0500

    Finished fixing views to remove all logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,48 +18,52 @@ module ApplicationHelper
 
   def user_index_friendship_status(user)
     if user.friend?(current_user)
-      content_tag(:span, ("You are friend with #{user.name}"), class: 'user-friend')
+      content_tag(:span, "You are friend with #{user.name}", class: 'user-friend')
     elsif current_user.pending_friends.include?(user)
-      content_tag(:span, ("Your friend request is pending"), class: 'user-invite')
+      content_tag(:span, 'Your friend request is pending', class: 'user-invite')
     elsif user.id != current_user.id
-      content_tag(:span, (link_to 'Invite', invitation_path(:user_id => user.id), method: :post), class: 'profile-link', method: :post)
+      content_tag(:span, (link_to 'Invite', invitation_path(user_id: user.id),
+                                  method: :post), class: 'profile-link', method: :post)
     end
   end
 
   def pending_friends_request(user)
-    if user.id == current_user.id
-      users = []
-      unless user.pending_friends.empty?
-        user.pending_friends.each do |user|
-          users << user
-        end
-        render 'user_friend_requests', users: users
-      end
+    return unless user.id == current_user.id || !user.pending_friends.empty?
+
+    users = []
+    return if user.pending_friends.empty?
+
+    user.pending_friends.each do |user_request|
+      users << user_request
     end
+    render 'user_friend_requests', users: users
   end
 
   def friend_requests(user)
-    if user.id == current_user.id
-      users = []
-      unless user.friend_requests.empty?
-        user.friend_requests.each do |user|
-          users << user
-        end 
-        render 'user_pending_invites', users: users
-      end
+    return unless user.id == current_user.id
+
+    users = []
+    return if user.friend_requests.empty?
+
+    user.friend_requests.each do |user_friend|
+      users << user_friend
     end
+    render 'user_pending_invites', users: users
   end
 
   def post_rendering(user)
     if user.posts.empty?
-	  content_tag(:h3, 'You have no posts yet!')
-	 
+      content_tag(:h3, 'You have no posts yet!')
     else
       posts = []
       user.posts.each do |post|
-		posts << post 
-	  end
-	  render 'posts/user_show_post', posts: posts
+        posts << post
+      end
+      render 'posts/user_show_post', posts: posts
     end
+  end
+
+  def timeline_post(posts)
+    render 'posts/user_show_post', posts: posts
   end
 end
